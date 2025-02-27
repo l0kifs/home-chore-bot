726872ab2790b0335c161902352865243fb4f4c7
Viktoriia Kutseva <Kutsevaviktoriia@gmail.com>
2025-02-27 23:02:35 +0600

    upd

--- app/config/env_vars.py
+++ app/config/env_vars.py
@@ -4,8 +4,6 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class EnvVars(BaseSettings):
     TELEGRAM_BOT_TOKEN: str
     DB_URL: str  # Add your database URL here or any other required variables
- 
-
 
     model_config = SettingsConfigDict(env_file='.env')
 
